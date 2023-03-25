fce2fdbcfe72d773bff02a68ac996ba01ce2a9df
MD NOUSHAD ANSARI <razamd260@gmail.com>
2023-03-25 07:51:22 +0530

    POM1

---
+++ src/POM/POM_2.java
@@ -0,0 +1,24 @@
+package POM;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+import First.POM_1;
+
+public class POM_2 {
+	WebDriver driver;
+	@Test
+	public void Facebook()
+	{
+		ChromeOptions option=new ChromeOptions();
+		option.addArguments("--disable-notifications");
+		driver=new ChromeDriver(option);
+		driver.get("https://www.facebook.com/");
+		POM_1 p=new POM_1(driver);
+		p.setid("alinoushad260@gmail.com");
+		p.setpwd("razamd260@G");
+		p.setlogin();
+	}
+}
